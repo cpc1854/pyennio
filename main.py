fcb69fa20f20f3f47b1ea1ff56c066db5660ccce
cpc1854 <cpc1854@gmail.com>
2018-09-23 09:28:45 +0800

    2

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
+import urllib
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 app = Flask(__name__)
 
+def get_webpage(url):
+    #html = myHtml.get_webpage(url)
+    req = urllib.request.Request(url , headers={'User-Agent': 'Mozilla/5.0'})
+    return urllib.request.urlopen(req).read()
+
+@app.route('/')
+def hello_world():
+    url = 'http://feeds.bbci.co.uk/news/world/rss.xml'
+    xml = get_webpage(url)
+    return xml
+
+'''
 @app.route('/')
 def index():
     return render_template(
         'indexh.html',**locals())
+'''
 ##def hello_world():
 ##  return 'Hey its Python Flask application on Azure!'
 if __name__ == '__main__':
